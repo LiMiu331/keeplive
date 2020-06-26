ed7cf6d795664c2585441d14a5afaef15c09b1a6
limiu <845217637@qq.com>
2020-06-26 13:03:43 +0800

    deploy

---
+++ precache-manifest.eb21c404c6ae4c92d161080851e3d047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3171303c6bf1aaa7fda",
+    "url": "/keeplive/css/app.fc24b8ec.css"
+  },
+  {
+    "revision": "d0c578d9642e509c7c17",
+    "url": "/keeplive/css/chunk-vendors.4f0fe6bb.css"
+  },
+  {
+    "revision": "6867fc3dc63519cd63596f63f379a606",
+    "url": "/keeplive/index.html"
+  },
+  {
+    "revision": "d3171303c6bf1aaa7fda",
+    "url": "/keeplive/js/app.e9152cc7.js"
+  },
+  {
+    "revision": "d0c578d9642e509c7c17",
+    "url": "/keeplive/js/chunk-vendors.edd107ad.js"
+  },
+  {
+    "revision": "286846ef2fa3a58d5842e20f8300fcae",
+    "url": "/keeplive/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keeplive/robots.txt"
+  }
+]);
